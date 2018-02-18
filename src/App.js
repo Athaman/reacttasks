94c3632fe660d5fa91707be0ac3c816bba7f2883
Keone Martin <keonemartin@gmail.com>
2018-02-18 15:35:52 +1100

    basic tasks outline added and retrieved tasks from db

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import {Appbar, Container} from 'muicss/react';
+import Tasks from './components/Tasks';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      tasks: []
+    }
+  }
+
+  componentWillMount(){
+    this.getTasks();
+  }
+
+  getTasks(){
+    axios.request({
+      method: 'get',
+      //  obviously don't include an api key inline for production
+      url: 'https://api.mlab.com/api/1/databases/reacttasks/collections/tasks?apiKey=Ymr0S92WExflVUa1qsP_3Xlk3MTGYNCp'
+    }).then((response) => {
+      this.setState({tasks: response.data}, () => {
+        console.log(this.state);
+      });
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Appbar>
+          <Container>
+            <table width="100%">
+              <tbody>
+                <tr>
+                  <td className="mui--appbar-height"><h3>ReactTasks</h3></td>
+                </tr>
+              </tbody>
+            </table>
+          </Container>
+        </Appbar>
+        <br />
+        <Container>
+          <Tasks tasks={this.state.tasks}/>
+        </Container>
       </div>
     );
   }
---
+++ src/components/Tasks.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {Panel} from 'muicss/react';
+import TaskItem from './TaskItem';
+
+class Tasks extends Component {
+  render() {
+    let taskItems;
+    if(this.props.tasks){
+      taskItems = this.props.tasks.map(task => {
+        return (
+          <TaskItem key={task._id.$oid} task={task} />
+        )
+      });
+    }
+    return (
+      <Panel>
+        {taskItems}
+      </Panel>
+    );
+  }
+}
+
+export default Tasks;
